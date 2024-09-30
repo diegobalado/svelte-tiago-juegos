dd9d750b997bafd756928c1136a2c7b8e2393160
Diego <jdiegomdq@gmail.com>
2024-09-30 14:11:21 -0300

    add answer checking

--- src/routes/emojis/+page.svelte
+++ src/routes/emojis/+page.svelte
@@ -2,6 +2,7 @@
 	import { onMount } from 'svelte';
 	import Background from '../../components/Background.svelte';
 	import { random } from 'node-emoji';
+	import { writable } from 'svelte/store';
 
 	/**
 	 * @type {{
@@ -9,7 +10,11 @@
 	 *   emoji: string
 	 * } | null }
 	 */
+
 	let randomEmoji;
+
+	let respondido = writable(false);
+	let correcto = writable(false);
 
 	onMount(() => {
 		randomEmoji = random();
@@ -19,8 +24,14 @@
 	const filas = 8;
 	const cantidad = filas * columnas;
 	const emojiDistinto = Math.floor(Math.random() * cantidad);
-	const getEmojiClasses = (i = 0) =>
-		`emoji ${i === emojiDistinto ? 'distinto' : ''}`;
+	const getEmojiClasses = (i = 0) => `emoji ${i === emojiDistinto ? 'distinto' : ''}`;
+	const handleClick = (i = 0) => {
+		$respondido = true;
+		$correcto = i === emojiDistinto;
+	};
+	const handleReload = () => {
+		location.reload();
+	};
 </script>
 
 <svelte:head>
@@ -30,17 +41,28 @@
 
 <div class="absolute h-full w-full overflow-hidden bg-gradient-to-r from-slate-900 to-slate-700">
 	<Background />
-	<div class="flex w-full h-full items-center justify-center z-10 relative">
+	<div class="flex flex-col w-full h-full items-center justify-center z-10 relative">
 		<div class="grid grid-cols-6 md:grid-cols-8 gap-2 md:gap-8">
 			{#if randomEmoji}
 				{#each Array.from({ length: cantidad }) as _, i}
-					<p class={getEmojiClasses(i)}>
-						{randomEmoji.emoji}
-					</p>
+					<button on:click={() => handleClick(i)}>
+						<p class={getEmojiClasses(i)}>
+							{randomEmoji.emoji}
+						</p>
+					</button>
 				{/each}
 			{:else}
 				<p class="text-white text-2xl">Cargando...</p>
 			{/if}
 		</div>
+		{#if $respondido}
+			<button on:click={() => handleReload()} class="btn btn-active btn-accent">
+				{#if $correcto}
+					<p>Correcto!! 🎉🎉</p>
+				{:else}
+					<p>NOOO!! 💩💩💩</p>
+				{/if}
+			</button>
+		{/if}
 	</div>
 </div>
